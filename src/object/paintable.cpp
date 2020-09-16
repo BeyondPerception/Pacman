0711263e71f3a0ed3d8e8ab125db3e58555c7046
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-15 20:39:20 -0500

    Add paintable implementation

---
+++ src/object/paintable.cpp
@@ -0,0 +1,20 @@
+#include <object/paintable.h>
+
+Paintable::Paintable(SDL_Renderer* renderer, const std::string& path, int x, int y) {
+	texture = IMG_LoadTexture(renderer, path.c_str());
+	if (texture == nullptr) {
+		std::cout << "Failed to load image path: " << path << "! Error: " << SDL_GetError() << "\n";
+		exit(1);
+	}
+	this->renderer = renderer;
+	this->x = x;
+	this->y = y;
+}
+
+
+void Paintable::paint() {
+	SDL_Rect pos = {.x=x, .y=y};
+	SDL_QueryTexture(texture, nullptr, nullptr, &pos.w, &pos.h);
+	SDL_RenderCopy(renderer, texture, nullptr, &pos);
+}
+
