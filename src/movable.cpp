a027d2e02a353b80e8f94293e0916a3eb301cb18
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-15 21:21:19 -0500

    I def forgot to add this file in the last commit again

---
+++ src/movable.cpp
@@ -0,0 +1,41 @@
+#include <movable.h>
+
+void Movable::move(Direction dir) {
+	lastDirection = dir;
+	switch (dir) {
+		case UP:
+			if (y - TEXTURE_SIZE / 4 >= bounds.y) {
+				setY(y - TEXTURE_SIZE / 4);
+			}
+			break;
+		case DOWN:
+			if (y + TEXTURE_SIZE / 4 <= bounds.y + bounds.h) {
+				setY(y + TEXTURE_SIZE / 4);
+			}
+			break;
+		case LEFT:
+			if (x - TEXTURE_SIZE / 4 >= bounds.x) {
+				setX(x - TEXTURE_SIZE / 4);
+			}
+			break;
+		case RIGHT:
+			if (x + TEXTURE_SIZE / 4 <= bounds.x + bounds.w) {
+				setX(x + TEXTURE_SIZE / 4);
+			}
+			break;
+	}
+}
+
+void Movable::setX(int x) {
+	setXY(x, y);
+}
+
+void Movable::setY(int y) {
+	setXY(x, y);
+}
+
+void Movable::setXY(int x, int y) {
+	this->x = x;
+	this->y = y;
+	paint();
+}
