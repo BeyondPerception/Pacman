1479efed25e3cebd2c93d05bb72a2f437d4c858c
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-15 21:18:49 -0500

    Move x and y into the protected section so movable can change them as well as store the width and height when it is queried

--- include/object/paintable.h
+++ include/object/paintable.h
@@ -6,10 +6,16 @@
 #include <SDL_image.h>
 #include <iostream>
 
+#define TEXTURE_SIZE 16
+
 class Paintable {
 	SDL_Texture* texture;
 	SDL_Renderer* renderer;
 
+	int w{};
+	int h{};
+protected:
+	// DO NOT CHANGE THESE VALUES MANUALLY.
 	int x;
 	int y;
 public:
