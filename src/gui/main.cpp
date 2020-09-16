f7c3e2b6ace76e71a51c5c0641e61c948bc89784
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-15 22:02:29 -0500

    Update main testing file

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,14 +1,22 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
 #include <thread>
+#include <gui/colors.h>
+#include <object/movable.h>
 
-const int SCREEN_WIDTH = 1080;
+const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 720;
 
 SDL_Window* init() {
 	// Initialize SDL2
 	if (SDL_Init(SDL_INIT_VIDEO)) {
 		std::cout << "Failed to initialize SDL! Error: " << SDL_GetError() << "\n";
+		exit(1);
+	}
+
+	if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+		std::cout << "Failed to initialize SDL_image! Error: " << IMG_GetError() << "\n";
 		exit(1);
 	}
 
@@ -34,19 +42,36 @@ int main() {
 
 	SDL_Window* window = init();
 
-	// This is what we will draw on
-	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr) {
+		std::cout << "Could not create renderer! Error: " << SDL_GetError() << "\n";
+		exit(1);
+	}
+	SDL_SetRenderDrawColor(renderer, COLOR_BACKGROUND >> 16 & 0xFF, COLOR_BACKGROUND >> 8 & 0xFF,
+						   COLOR_BACKGROUND & 0xFF, 0xFF);
+
+	Movable pacman(renderer, "../assets/pacman_small.png", 0, 10, SDL_Rect{0, 0, SCREEN_WIDTH, SCREEN_HEIGHT});
 
 	SDL_Event curEvent;
+
+	std::this_thread::sleep_for(5s);
 
 	// so this is our event loop that will update the screen, move the ghosts, handle keyboard events, etc.
 	// Hopefully we can make it relatively short and abstract most of the code elsewhere
 	bool quit = false;
 	while (!quit) {
-		// We refill the window every loop because sometimes it can bug out if you move it around to quickly
-		SDL_FillRect(surface, nullptr, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-		// This is basically like a call to repaint() in java
-		SDL_UpdateWindowSurface(window);
+		SDL_RenderPresent(renderer);
+
+		if (pacman.isAtXBounds()) {
+			if (pacman.getLastDirection() == LEFT) {
+				pacman.move(RIGHT);
+			} else {
+				pacman.move(LEFT);
+			}
+		} else {
+			pacman.move(pacman.getLastDirection());
+		}
+		printf("x: %d y: %d\n", pacman.getX(), pacman.getY());
 
 		// PollEvent puts the next event into curEvent
 		while (SDL_PollEvent(&curEvent) != 0) {
