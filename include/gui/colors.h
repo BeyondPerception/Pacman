5e2ddc601736a0a5fd17c9474d4d9793a76ebe69
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-24 19:42:43 -0500

    Update background color

--- include/gui/colors.h
+++ include/gui/colors.h
@@ -6,7 +6,7 @@
 // Just some random ass colors I thought would look good
 // R = # >> 16 & 0xFF; G = # >> 8 & 0xFF; B = # & 0xFF
 
-#define COLOR_BACKGROUND  0x1B006Bu
+#define COLOR_BACKGROUND  0x0A004A
 #define COLOR_FOREGROUND  0x00E078u
 #define COLOR_PACMAN  0xFCF403u
 #define COLOR_ORANGE  0xF5CB42u
