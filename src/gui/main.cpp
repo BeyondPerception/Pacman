c40c88d4799215a865d9ef93a29556595f83abd5
Ronak Malik <ronakmalik4242@gmail.com>
2020-09-30 08:18:38 -0500

    Add colission detection between pacman and walls

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -55,10 +55,11 @@ int main() {
 	// This creates pacman
 	Movable pacman(renderer, "../assets/pacman_small.png", 16, 16, SDL_Rect{0, 0, SCREEN_WIDTH, SCREEN_HEIGHT});
 
-	GameBoard gameBoard(renderer, SCREEN_WIDTH / TEXTURE_SIZE, SCREEN_HEIGHT / TEXTURE_SIZE);
+	GameBoard gameBoard(renderer, SCREEN_HEIGHT / TEXTURE_SIZE, SCREEN_WIDTH / TEXTURE_SIZE);
 	gameBoard.generate();
 
 	SDL_Event curEvent;
+
 	// so this is our event loop that will update the screen, move the ghosts, handle keyboard events, etc.
 	// Hopefully we can make it relatively short and abstract most of the code elsewhere
 	bool quit = false;
@@ -66,7 +67,8 @@ int main() {
 		SDL_RenderPresent(renderer);
 
 		// PollEvent puts the next event into curEvent
-		while (SDL_PollEvent(&curEvent) != 0) {
+		while (pacman.getX() % TEXTURE_SIZE == 0 && pacman.getY() % TEXTURE_SIZE == 0 &&
+			   SDL_PollEvent(&curEvent) != 0) {
 			// Handle quit events so we can actually close the window without SIGTERM/SIGABRT
 			if (curEvent.type == SDL_QUIT) {
 				quit = true;
@@ -74,6 +76,7 @@ int main() {
 			} else if (curEvent.type == SDL_KEYDOWN) {
 				// Handle keyboard input
 				SDL_Keycode keycode = curEvent.key.keysym.sym;
+
 				switch (keycode) {
 					case SDLK_UP:
 						pacman.setDirection(UP);
@@ -94,13 +97,57 @@ int main() {
 		}
 
 		// Keep moving pacman forward if the user has not changed dirs.
-		pacman.move(pacman.getLastDirection());
+		Point p = gameBoard.getPos(pacman.getY(), pacman.getX());
+//		std::cout << (int) p.r << " " << (int) p.c << "\n";
+
+		for (int i = 0; i < gameBoard.getRows(); i++) {
+			for (int j = 0; j < gameBoard.getCols(); j++) {
+				if (p.r == i && p.c == j) {
+					std::cout << "O";
+				} else if (gameBoard[i][j] == nullptr) {
+					std::cout << ".";
+				} else {
+					std::cout << "#";
+				}
+			}
+			std::cout << "\n";
+		}
+		std::cout << "\n";
+
+		if (pacman.getX() % TEXTURE_SIZE == 0 && pacman.getY() % TEXTURE_SIZE == 0) {
+			switch (pacman.getLastDirection()) {
+				case UP:
+					if (gameBoard[p.r - 1][p.c] == nullptr) {
+						pacman.move(pacman.getLastDirection());
+					}
+					break;
+				case DOWN:
+					if (gameBoard[p.r + 1][p.c] == nullptr) {
+						pacman.move(pacman.getLastDirection());
+					}
+					break;
+				case LEFT:
+					if (gameBoard[p.r][p.c - 1] == nullptr) {
+						pacman.move(pacman.getLastDirection());
+					}
+					break;
+				case RIGHT:
+					if (gameBoard[p.r][p.c + 1] == nullptr) {
+						pacman.move(pacman.getLastDirection());
+					}
+					break;
+				default:
+					break;
+			}
+		} else {
+			pacman.move(pacman.getLastDirection());
+		}
 
 		// slow the loop speed to something like 30fps (idk i'm bad at math, you should probably check this)
 		std::this_thread::sleep_for(33ms);
 	}
 
-// Cleanup
+	// Cleanup
 	cleanup(window);
 	return 0;
 }
